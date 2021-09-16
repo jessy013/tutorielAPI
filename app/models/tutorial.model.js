4a6bb1613f3c9f07174eedac75373fb0199e98e6
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-09-16 11:28:52 +0200

    up

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -3,6 +3,9 @@ module.exports = (sequelize, Sequelize) =>{
       title: {
           type: Sequelize.STRING
       },
+      description: {
+          type: Sequelize.STRING
+      },
       published:{
           type: Sequelize.BOOLEAN
       }  
